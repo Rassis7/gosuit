02b9c2f9728777dd38fe32c8d10ce70c52402d9b
Romulo Assis <romuloassis.as@gmail.com>
2019-01-25 13:00:19 -0200

    Iniciando o Gosuit admin na branch

---
+++ admin-front/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <q-toolbar
+        color="primary"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+      <q-btn
+        flat
+        dense
+        round
+        @click="toggle"
+        aria-label="Menu"
+      >
+        <q-icon name="menu" />
+      </q-btn>
+
+      <q-toolbar-title>
+          Quasar App
+          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
+        </q-toolbar-title>
+    </q-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarComponent',
+  data () {
+    return {}
+  },
+  methods: {
+    toggle () {
+      this.$emit('toogleMenuPai')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin-front/src/router/routes.js
@@ -0,0 +1,27 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }
+    ]
+  },
+  {
+    path: '/home',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
