b833c687dc761a0ca2cc107bec1d59d1e3187ff7
Romulo Assis <romulo.gviva@gmail.com>
2019-02-01 14:06:20 -0200

    Fazendo os modais bases do cadastro de mesa

---
+++ admin-front/src/components/Mesa/ModalCadastroLote.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <q-modal v-model="opendComputed" :content-css="{minWidth: '50vw', minHeight: '30vh'}">
+      <q-modal-layout>
+        <q-toolbar slot="header">
+          <q-toolbar-title>
+            Cadastro em lote de mesas
+          </q-toolbar-title>
+        </q-toolbar>
+
+        <div class="modal-body">
+
+          <div class="form row gutter-md">
+            <div class="col-4">
+              <q-input v-model="praca" float-label="Qual a praça da mesa?"/>
+            </div>
+
+            <div class="col-4">
+              <q-input v-model="Identificacao" float-label="Nome/Número da mesa"/>
+            </div>
+
+            <div class="col-4">
+              <q-input v-model="quantidadeCriadas" float-label="Deseja criar quantas mesas em lote?" type="number"/>
+            </div>
+          </div>
+
+        </div>
+
+        <q-toolbar slot="footer" color="white">
+            <q-btn color="faded" label="Fechar" class="float-right on-right" @click="resetarModal"/>
+            <q-btn color="primary" label="Salvar" class="float-right"/>
+        </q-toolbar>
+
+      </q-modal-layout>
+    </q-modal>
+  </div>
+</template>
+
+<script>
+import BaseModal from '../BaseModal.vue'
+
+export default {
+  name: 'ModalCadastroLoteComponent',
+  extends: BaseModal,
+  methods: {
+    resetarModal () {
+      this.$emit('fecharModalPai')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
